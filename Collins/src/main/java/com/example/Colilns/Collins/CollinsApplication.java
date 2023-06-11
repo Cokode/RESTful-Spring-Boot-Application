140653c050f3943dd66ff51712fc8429a1b56f0e
Collins Mr <96517341+Cokode@users.noreply.github.com>
2023-06-11 07:04:32 +0200

    initial commit

---
+++ Collins/src/main/java/com/example/Colilns/Collins/CollinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Colilns.Collins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollinsApplication.class, args);
+	}
+
+}
---
+++ Collins/src/test/java/com/example/Colilns/Collins/CollinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Colilns.Collins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
