4de04581710bdd232bd1ab1d2901b4ccd2c3138e
Collins Mr <96517341+Cokode@users.noreply.github.com>
2023-06-15 00:15:58 +0200

    White space removed in CollinsApplication.java

--- Collins/src/main/java/com/example/Colilns/Collins/CollinsApplication.java
+++ Collins/src/main/java/com/example/Colilns/Collins/CollinsApplication.java
@@ -2,13 +2,9 @@ package com.example.Colilns.Collins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CollinsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CollinsApplication.class, args);
 	}
-
-
 }
